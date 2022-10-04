2d8c0fff854acbf0ce8a69de366080820c59bd0f
jim <Jimmanrique12@gmail.com>
2022-10-05 06:05:55 +0800

    111

--- Backend/Logout.php
+++ Backend/Logout.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
 require_once('../Classes/DBHandler.php');
+
 $useremail = $_SESSION["userEmail"];
 
-$DBHandler = new DBHandler();
-$DBHandler-> updateColumn("users","USERSTATUS","offline","email",$useremail);
+//$DBHandler = new DBHandler();
+//$DBHandler-> updateColumn("users","USERSTATUS","offline","email",$useremail);
 
 
 session_destroy();
