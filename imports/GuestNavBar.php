fed44841ae6e24f9fcbfc04e4569f605258313b3
JimJ0112 <68044290+JimJ0112@users.noreply.github.com>
2022-11-17 19:40:56 +0800

    about.php

---
+++ imports/GuestNavBar.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		Responder Requestboard
+	</title>
+	<link rel="stylesheet" type="text/css" href="css/GuestNavBar.css">
+</head>
+<body onload="init()">
+
+<!--<img src="img/b.jpg" id="RequestboardBackground"/>-->
+
+<!-- NavBar-->
+<div id="NavContainer">
+	<img src="img/logo.png" id="Nav-Logo" title="Home"> 
+	<ul id="NavBar">
+		
+
+	
+		<a href="Requestor_Transactions.php">
+			<li class="Nav-item" title="My Transactions">
+
+			</li>
+		</a>
+		<a href="Requestor_RequestBoard.php"> 
+			<li class="Nav-item" title="RequestBoard">
+
+			</li> 
+		</a>
+		<a href="Requestor_AvailableServices.php"> 
+			<li class="Nav-item" title="Available Services"> 
+
+			</li> 
+		</a>
+	
+		<a href="Messages.php">
+			<li class="Nav-item" title="My Messages">
+
+			</li>
+		</a>
+
+
+	
+
+		<span>
+		
+			<li class="Nav-item" title="Me" id="userInfoNav">
+		
+            <a href="Login.php"> Login </a>
+
+			</li>
+		</span>
+
+			<!--
+				<a href="Backend/Logout.php"><li class="Nav-item" id="LogOutButton" title="Log Out">Log out</li></a>
+			-->	
+	</ul>
+</div>
+
+
+
+
+
+	
+		
+	
+
+
+
+	
+
+</body>
+</html>
