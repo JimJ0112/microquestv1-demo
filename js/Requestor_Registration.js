6e404fbcd501cc6344210d073aaf50d3a2783f39
JimJ0112 <68044290+JimJ0112@users.noreply.github.com>
2022-12-09 01:29:00 +0800

    Update Requestor_Registration.js

--- js/Requestor_Registration.js
+++ js/Requestor_Registration.js
@@ -715,3 +715,74 @@ function validateEmail(email) {
     return re.test(email);
 }
 
+
+
+
+
+// gets all services 
+function getServices(){
+    
+    
+    var xmlhttp = new XMLHttpRequest();
+    
+  
+    
+
+    xmlhttp.open("POST", "Backend/Get_Specializations.php", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.onload = function() {
+        if (this.readyState === 4 || this.status === 200){ 
+           
+           
+  
+
+            var dataArray = this.response;
+            dataArray = JSON.parse(dataArray);
+          
+            dataArray = categories(dataArray);
+
+            console.log(dataArray);
+            setData(dataArray);
+
+     
+        }else{
+            console.log(err);
+        }      
+    };
+    
+    xmlhttp.send();
+    
+}// end of function
+
+
+//  extracting categories
+function categories(array){
+    var dataArray = array;
+    var number = dataArray.length;
+    var newData = [];
+ 
+
+
+    for(var i = 0; i<number; i++){
+  
+        newData[i] = dataArray[i]['serviceCategory']
+    }
+    newData.sort();
+
+    return newData;
+
+}
+
+// set data to dropdown
+
+function setData(array){
+    var dataArray = array;
+    var number = dataArray.length;
+    var specializations = document.getElementById('specialization');
+    for(var i=0; i<number;i++){
+        var option = document.createElement('option');
+        option.innerText = dataArray[i];
+        option.value = dataArray[i];
+        specializations.add(option);
+    }
+}
